5f86293ed583b503c6f4bcb340333daa40b249ba
Tom Van Boghout <tomvanboghout@Toms-Mac.local>
2016-10-02 21:35:29 +0200

    finalize server project, include websockets

---
+++ src/server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tomvanboghout on 02/10/16.
+ */
+
+import Server from 'socket.io';
+
+export default function startServer(store) {
+    const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
+}
